bfa428978167f8767baca65ca82995a67e1423cb
foly <crazyfolyme@gmail.com>
2018-09-27 14:25:13 +0200

    fix go json.unmarshal string->int

--- services/mult/main.go
+++ services/mult/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"strconv"
+	"encoding/json"
 )
 
 const APP_VERSION = "v1"
@@ -10,7 +12,25 @@ const APP_BASEPATH = "/api/" + APP_VERSION
 const APP_SERVICE = "name: mult, version: " + APP_VERSION
 
 type MultRequest struct {
-	Operands [2]int `json:"operands" binding:"required"`
+	Operands [2]FlexInt `json:"operands" binding:"required"`
+}
+
+type FlexInt int
+
+func (fi *FlexInt) UnmarshalJSON(b []byte) error {
+		if b[0] != '"' {
+			return json.Unmarshal(b, (*int)(fi))
+		}
+		var s string
+		if err := json.Unmarshal(b, &s); err != nil {
+			return err
+		}
+		i, err := strconv.Atoi(s)
+		if err != nil {
+			return err
+		}
+		*fi = FlexInt(i)
+		return nil
 }
 
 func main() {
@@ -27,7 +47,7 @@ func main() {
 
 		api.POST("/mult", func(c *gin.Context) {
 			var body MultRequest
-			
+
 			if err := c.ShouldBindJSON(&body); err != nil {
 				c.JSON(400, gin.H{
 					"error": "Invalid Input",
